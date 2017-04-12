3ecd4cb2869b20a549cb00d8e2f14cd77d626b86
!shan <ishan210788@gmail.com>
2017-04-12 13:21:05 +0200

    adding all files

---
+++ trafficBooster.js
@@ -0,0 +1,27 @@
+var path = require('path'),
+  async = require('async'), //https://www.npmjs.com/package/async
+  newman = require('newman'),
+
+  parametersForTestRun = {
+    collection: path.join(__dirname, './rmgKruxWebURL.postman_collection.json'), // your collection
+    //environment: path.join(__dirname, 'postman_environment.json'), //your env
+  };
+
+parallelCollectionRun = function(done) {
+  newman.run(parametersForTestRun, done);
+};
+
+// Runs the Postman sample collection thrice, in parallel.
+async.parallel([
+    parallelCollectionRun,
+  ],
+  function(err, results) {
+    err && console.error(err);
+
+    results.forEach(function(result) {
+      var failures = result.run.failures;
+      console.log(result)
+      console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
+        `${result.collection.name} ran successfully.`);
+    });
+  });
